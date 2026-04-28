60dff4a18f98af5c54c0f35e6aeae0bc31531abc
jwesterstahl <Jacob.westerstahl@gmail.com>
2026-04-28 10:00:39 -0400

    Work on HW4

---
+++ HW/2526-HW4/Source/Delay.cpp
@@ -0,0 +1,89 @@
+/*
+  ==============================================================================
+
+    Delay.cpp
+ 
+    This code contains the implementation needed for a simple feedback delay.
+
+  ==============================================================================
+*/
+
+#include "Delay.h"
+
+
+void Delay::prepare(double samplingRate, int maxDelay, int numChannels)
+{
+    sampleRate = samplingRate;
+
+    delayBufferSize = maxDelay;
+
+    delayBuffer.setSize(numChannels, delayBufferSize);
+    delayBuffer.clear();
+
+    // sets smoothing changes to linear smoothing in 10ms
+    smoothDelay.reset(sampleRate, 0.01);
+
+    writeHeads.resize(numChannels);
+    for (int c = 0; c < numChannels; ++c)
+    {
+        writeHeads[c] = 0;
+    }
+}
+
+void Delay::setMaxDelayInSamples(int maxDelay)
+{
+    maxDelayInSamples = maxDelay;
+}
+
+int Delay::getMaxDelayInSamples()
+{
+    return maxDelayInSamples;
+}
+
+void Delay::setDelayTime(float delaySeconds)
+{
+    smoothDelay.setTargetValue(delaySeconds);
+}
+
+void Delay::setWetMix(float wetAmount)
+{
+    mix = wetAmount;
+
+}
+
+// this is called in the ProcessBlock as we iterate over each channel's buffer
+
+float Delay::interpRead(float* delayData, int writeHead, float delaySamples)
+{
+    float readTail = std::fmod((writeHead - delaySamples + delayBufferSize), delayBufferSize);
+    if (readTail < 0)
+    {
+        readTail += delayBufferSize;
+    }
+
+    int before = floor(readTail);
+    int after = (before + 1) % delayBufferSize;
+
+    float frac = readTail - before;
+
+    return (delayData[before] * (1 - frac)) + (delayData[after] * frac);
+}
+
+float Delay::processSample(float inputSample, int channel)
+{
+    float* delayData = delayBuffer.getWritePointer(channel);
+
+    int writeHead = writeHeads[channel];
+    
+    float delayed = interpRead(delayData, writeHead, delaySamples);
+    
+    delayData[writeHead] = inputSample + (feedback * delayed);
+
+    delayed = ((1.0 - mix) * inputSample) + (mix * delayed);
+
+    writeHead = (writeHead + 1) % delayBufferSize;
+
+    writeHeads[channel] = writeHead;
+
+    return delayed;
+}
